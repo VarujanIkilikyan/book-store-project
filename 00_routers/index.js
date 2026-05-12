5d847c9a04ff6e3bdbad51c4334957fbc0b4c5ff
Varujan Ikilikyan <varujanna@gmail.com>
2026-05-12 18:12:55 +0400

    start auth router

--- 00_routers/index.js
+++ 00_routers/index.js
@@ -1,9 +1,11 @@
 import  {Router} from "express";
 
 import views from "../01_controllers/views.js";
+import authRouter from "./auth.js"
 
 const SelectorRouter = Router();
 
 SelectorRouter.get('/',views.viewRender('index'))
+SelectorRouter.use('/auth',authRouter)
 
 export default SelectorRouter;
